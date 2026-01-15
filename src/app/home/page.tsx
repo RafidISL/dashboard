651f31e67a4014c5a6d2c2067514131a9e774a96
Islam Rafid <14529836@itisrossi.vi.it>
2026-01-15 19:21:24 +0100

    dashboard

---
+++ src/app/home/page.tsx
@@ -0,0 +1,18 @@
+export default function HelloPage(){
+    return(
+        <div>
+            <h1>MATERIE:</h1>
+            <ul>
+                <li>Storia</li>
+                <li>Matematica</li>
+                <li>Inglese</li>
+                <li>Telecomunicazioni</li>
+                <li>Informatica</li>
+                <li>Sisteme e reti</li>
+                <li>Letteratura</li>
+                <li>Educazione Civica</li>
+            </ul>
+        </div>
+
+    );
+};
